192feec331fbfba9b013c84ea19e6fa1f0f95924
feng-crazy <894220128@qq.com>
2026-03-13 16:15:54 +0800

    local build

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+http_proxy=http://127.0.0.1:7890 https_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890 eas build --profile preview --platform android --local --non-interactive
